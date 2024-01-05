f14ffde6c6a3377d6b90f5e8436af9f8f163c730
Reginald Desrosiers <reginald.j.desrosiers@gmail.com>
2024-01-04 19:28:41 -0500

    get museum details, no styling

--- react-vite/src/components/MuseumDetailsPage/MuseumDetailsPage.jsx
+++ react-vite/src/components/MuseumDetailsPage/MuseumDetailsPage.jsx
@@ -18,11 +18,22 @@ const MuseumDetailsPage = () => {
         <div>
             <div>
                 {museum.length === 1 ? <img src={museum[0].image_url} /> : null}
-                <p><span>Home</span> <span>Museums</span> {museum.length === 1 ? <span>{museum[0].name}</span> : null }</p>
+                <p><NavLink to="/"><span>Home</span></NavLink> <NavLink to="/museums"><span>Museums</span></NavLink> {museum.length === 1 ? <span>{museum[0].name}</span> : null }</p>
                 {museum.length === 1 ? <h1>{museum[0].name}</h1>: null}
                 {museum.length === 1 ? <p>{museum[0].description}</p> : null}
                 <button>See all products</button>
             </div>
+            {museum.length === 1 ?
+            <div>
+                <h2>Find us at the shop</h2>
+                <div>
+                    <p>{museum[0].store_name}</p>
+                    <p>{museum[0].store_address}</p>
+                    {museum[0].phone_number && <p>Phone: {museum[0].phone_number}</p>}
+                    {museum[0].email && <p>{museum[0].email}</p>}
+                    <NavLink to={museum[0].museum_website} target="_blank"><button>VISIT THE MUSEUM WEBSITE</button></NavLink>
+                </div>
+            </div> : null}
         </div>
     )
 }
