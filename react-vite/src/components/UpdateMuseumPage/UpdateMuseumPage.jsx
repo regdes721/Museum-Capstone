410227c01c992df73588385fb6225fa7902abcab
Reginald Desrosiers <reginald.j.desrosiers@gmail.com>
2024-01-07 12:13:38 -0500

    partial update museum styling (layout)

--- react-vite/src/components/UpdateMuseumPage/UpdateMuseumPage.jsx
+++ react-vite/src/components/UpdateMuseumPage/UpdateMuseumPage.jsx
@@ -61,49 +61,49 @@ export default function UpdateMuseumPage() {
     if (!sessionUser) return null
 
     return (
-        <form onSubmit={onSubmit} encType="multipart/form-data">
+        <form onSubmit={onSubmit} encType="multipart/form-data" className='create-museum-container'>
             <div>
                 <h1>{`What's the name of your museum?`}</h1>
-                <span>{errors.name}</span>
-                <input type='text' value={name} onChange={e => setName(e.target.value)} placeholder="Ex: Museum of Modern Art " />
+                <input type='text' value={name} onChange={e => setName(e.target.value)} placeholder="Ex: Museum of Modern Art " className='create-museum-input' />
+                <p className='red'>{errors.name}</p>
             </div>
             <div>
                 <h1>Describe your museum</h1>
-                <span>{errors.description}</span>
-                <input type='text' value={description} onChange={e => setDescription(e.target.value)} placeholder="Ex: MoMA is a place that fuels creativity, ignites minds, and provides inspiration with its extraordinary exhibitions and collection of modern and contemporary art." />
+                <textarea rows="13" cols="70" type='text' value={description} onChange={e => setDescription(e.target.value)} placeholder="Ex: MoMA is a place that fuels creativity, ignites minds, and provides inspiration with its extraordinary exhibitions and collection of modern and contemporary art." />
+                <p className='red'>{errors.description}</p>
             </div>
             <div>
                 <h1>Upload an image for your museum</h1>
-                <span>{errors.image_url}</span>
-                <input type='text' value={image_url} onChange={e => setImage(e.target.value)} />
+                <input type='text' value={image_url} onChange={e => setImage(e.target.value)} className='create-museum-input' />
+                <p className='red'>{errors.image_url}</p>
                 {/* <input type="file" className='server-creation-file-upload' accept="image/*" onChange={(e) => setImage(e.target.files[0])} /> */}
             </div>
             <div>
-                <h1>What is the name of your museum's store/gift shop?</h1>
-                <span>{errors.store_name}</span>
-                <input type='text' value={store_name} onChange={e => setStoreName(e.target.value)} placeholder="Ex: MoMA Design Store" />
+                <h1>{`What is the name of your museum's store/gift shop?`}</h1>
+                <input type='text' value={store_name} onChange={e => setStoreName(e.target.value)} placeholder="Ex: MoMA Design Store" className='create-museum-input' />
+                <p className='red'>{errors.store_name}</p>
             </div>
             <div>
-                <h1>What is the address of your museum's store/gift shop?</h1>
-                <span>{errors.store_address}</span>
-                <input type='text' value={store_address} onChange={e => setStoreAddress(e.target.value)} placeholder="Ex: 11 West 53 Street, Manhattan, NY 10019" />
+                <h1>{`What is the address of your museum's store/gift shop?`}</h1>
+                <input type='text' value={store_address} onChange={e => setStoreAddress(e.target.value)} placeholder="Ex: 11 West 53 Street, Manhattan, NY 10019" className='create-museum-input' />
+                <p className='red'>{errors.store_address}</p>
             </div>
             <div>
-                <h1>Add a phone number for your museum's store/gift shop. (optional)</h1>
-                <span>{errors.phone_number}</span>
-                <input type='text' value={phone_number} onChange={e => setPhoneNumber(e.target.value)} />
+                <h1>{`Add a phone number for your museum's store/gift shop. (optional)`}</h1>
+                <input type='text' value={phone_number} onChange={e => setPhoneNumber(e.target.value)} className='create-museum-input' />
+                <p className='red'>{errors.phone_number}</p>
             </div>
             <div>
-                <h1>Add an email address for your museum's store/gift shop. (optional)</h1>
-                <span>{errors.email}</span>
-                <input type='text' value={email} onChange={e => setEmail(e.target.value)} />
+                <h1>{`Add an email address for your museum's store/gift shop. (optional)`}</h1>
+                <input type='text' value={email} onChange={e => setEmail(e.target.value)} className='create-museum-input' />
+                <p className='red'>{errors.email}</p>
             </div>
             <div>
-                <h1>What is your museum's website?</h1>
-                <span>{errors.museum_website}</span>
-                <input type='text' value={museum_website} onChange={e => setMuseumWebsite(e.target.value)} placeholder="Ex: https://www.moma.org/" />
+                <h1>{`What is your museum's website?`}</h1>
+                <input type='text' value={museum_website} onChange={e => setMuseumWebsite(e.target.value)} placeholder="Ex: https://www.moma.org/" className='create-museum-input' />
+                <p className='red'>{errors.museum_website}</p>
             </div>
-            <input type='submit' value="Update Museum" />
+            <input type='submit' value="Create New Museum" className='create-museum-submit-button'/>
             {!sessionUser && (
                 <Navigate to="/" replace={true} />
             )}
