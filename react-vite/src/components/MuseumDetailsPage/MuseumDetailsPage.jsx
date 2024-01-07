adc3a2c4e7da694096aa537218930947e44cd1bc
Reginald Desrosiers <reginald.j.desrosiers@gmail.com>
2024-01-07 11:03:11 -0500

    museum details styling (layout)

--- react-vite/src/components/MuseumDetailsPage/MuseumDetailsPage.jsx
+++ react-vite/src/components/MuseumDetailsPage/MuseumDetailsPage.jsx
@@ -20,25 +20,31 @@ const MuseumDetailsPage = () => {
     }, [dispatch, museumId])
 
     return (
-        <div>
+        <div className="museum-details-container">
             <div>
-                {museum.length === 1 ? <img src={museum[0].image_url} /> : null}
-                <p><NavLink to="/"><span>Home</span></NavLink> <NavLink to="/museums"><span>Museums</span></NavLink> {museum.length === 1 ? <span>{museum[0].name}</span> : null }</p>
-                {museum.length === 1 ? <h1>{museum[0].name}</h1>: null}
-                {museum.length === 1 ? <p>{museum[0].description}</p> : null}
-                <div className={organizerButtonClassName}>
-                    {museum.length === 1 ? <NavLink to={`/museums/${museum[0].id}/edit`}><button>Update Museum</button></NavLink> : null}
-                    <OpenModalButton
-                        buttonText="Delete Museum"
-                        modalComponent={<DeleteMuseumModal />}
-                    />
+                {museum.length === 1 ? <img src={museum[0].image_url} className="museum-details-img" /> : null}
+                <div className="museum-details-main-container">
+                    <p><NavLink to="/"><span> <i className="fa-solid fa-angle-left"></i> Home</span></NavLink> <NavLink to="/museums"><span>Museums</span></NavLink> {museum.length === 1 ? <span>{museum[0].name}</span> : null }</p>
+                    {museum.length === 1 ? <h1>{museum[0].name}</h1>: null}
+                    <div className="museum-details-columns-container">
+                        {museum.length === 1 ? <p>{museum[0].description}</p> : null}
+                        <div className="museum-details-column-right museum-details-buttons-right">
+                            <div className={`${organizerButtonClassName} museum-details-buttons-right`}>
+                                {museum.length === 1 ? <NavLink to={`/museums/${museum[0].id}/edit`}><button>Update Museum</button></NavLink> : null}
+                                <OpenModalButton
+                                    buttonText="Delete Museum"
+                                    modalComponent={<DeleteMuseumModal />}
+                                />
+                            </div>
+                            <button className="museum-details-products-button"><p>See all products</p> <i className="fa-solid fa-arrow-right"></i></button>
+                        </div>
+                    </div>
                 </div>
-                <button>See all products</button>
             </div>
             {museum.length === 1 ?
             <div>
                 <h2>Find us at the shop</h2>
-                <div>
+                <div className="museum-details-shopcard-container">
                     <p>{museum[0].store_name}</p>
                     <p>{museum[0].store_address}</p>
                     {museum[0].phone_number && <p>Phone: {museum[0].phone_number}</p>}
