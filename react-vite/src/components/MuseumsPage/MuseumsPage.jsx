bbc02b6cefdd0a8f713c8e18d4f3f92246da39d0
Reginald Desrosiers <reginald.j.desrosiers@gmail.com>
2024-01-10 18:15:54 -0500

    styled get all museums page

--- react-vite/src/components/MuseumsPage/MuseumsPage.jsx
+++ react-vite/src/components/MuseumsPage/MuseumsPage.jsx
@@ -16,13 +16,13 @@ const MuseumsPage = () => {
     return (
         <div className="all-museums-container">
             <div className="all-museums-header">
-                <NavLink to="/"><p className="all-museums-header-p"><i className="fa-solid fa-angle-left"></i> Home</p></NavLink>
-                <h1>Museums</h1>
-                <h3>Discover the selection of books and products from the most famous Museums.</h3>
+                <NavLink to="/" className='no-underline font-text'><p className="all-museums-header-p"><i className="fa-solid fa-angle-left"></i> Home</p></NavLink>
+                <h1 className="font-header all-museums-header-title">Museums</h1>
+                <h3 className="font-text all-museums-header-text">Discover the selection of books and products from the most famous Museums.</h3>
             </div>
-            <div className="all-museums-grid-container">
+            <div className="all-museums-grid-container font-text">
                 {museums.map((museum) => (
-                    <NavLink to={`/museums/${museum.id}`}>
+                    <NavLink to={`/museums/${museum.id}`} className='no-underline'>
                         <div className="all-museums-spot-container">
                             <img src={museum.image_url} />
                             <h2>{museum.name} <i className="fa-solid fa-arrow-right"></i></h2>
