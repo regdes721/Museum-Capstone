5f6551898f5197c64ca349eb8f6a8800ed938b3c
Reginald Desrosiers <reginald.j.desrosiers@gmail.com>
2024-01-10 19:32:50 -0500

    styled museum best sellers page

--- react-vite/src/components/MuseumBestSellersPage/MuseumBestSellersPage.jsx
+++ react-vite/src/components/MuseumBestSellersPage/MuseumBestSellersPage.jsx
@@ -2,6 +2,7 @@ import { useEffect } from "react"
 import { NavLink, useParams } from "react-router-dom"
 import { useDispatch, useSelector } from "react-redux"
 import { thunkLoadMuseumDetails} from "../../redux/museums";
+import './MuseumBestSellersPage.css'
 
 export default function MuseumBestSellersPage() {
     const { museumId } = useParams()
@@ -30,18 +31,40 @@ export default function MuseumBestSellersPage() {
     }, [dispatch, museumId])
 
     return (
-        <div>
-            <div>
-                <p><NavLink to="/"><span> <i className="fa-solid fa-angle-left"></i> Home</span></NavLink> <NavLink to="/museums"><span>Museums</span></NavLink> {museum.length === 1 ? <NavLink to={`/museums/${museum[0].id}`}><span>{museum[0].name}</span></NavLink> : null }</p>
-                <h1>Best Sellers</h1>
+        <div className="all-museums-container">
+            <div className="museums-best-sellers-header">
+                <p><NavLink to="/" className='no-underline font-text'><span> <i className="fa-solid fa-angle-left"></i> Home</span></NavLink> <NavLink to="/museums" className='no-underline font-text'><span>Museums</span></NavLink> {museum.length === 1 ? <NavLink to={`/museums/${museum[0].id}`} className='no-underline font-text'><span>{museum[0].name}</span></NavLink> : null }</p>
+                <h1 className="font-header all-museums-header-title">Best Sellers {museum[0]?.name}</h1>
             </div>
-            {sortedBestSellers && sortedBestSellers.length > 0 && sortedBestSellers.map((product) =>
-                <NavLink to={`/products/${product.id}/details`}><div>
-                    <img src={product?.product_images[0].image_url} />
-                    <h3>{product?.name}</h3>
-                    <p>€{product?.price}</p>
+            <div className="best-sellers-preview-grid font-text">
+                {sortedBestSellers && sortedBestSellers.length > 0 && sortedBestSellers.map((product) =>
+                <NavLink to={`/products/${product.id}/details`} className='no-underline'><div className="best-sellers-preview-container">
+                    <img src={product?.product_images[0].image_url} className="best-sellers-preview-img" />
+                    <div className="best-sellers-preview-name">
+                        <h3>{product?.name}</h3>
+                    </div>
+                    <p>€{product?.price.toFixed(2)}</p>
                 </div></NavLink>
                 )}
+            </div>
+            <div className="product-guarantees-infocard-container font-text">
+                <div className="product-guarantees-infocard-details">
+                    <h3><i className="fa-solid fa-check"></i> Delivery</h3>
+                    <p>Shipping in 1-2 business days according to the method of delivery chosen</p>
+                </div>
+                <div>
+                    <h3><i className="fa-solid fa-check"></i> Satisfied?</h3>
+                    <p>14 days to change your mind</p>
+                </div>
+                <div>
+                    <h3><i className="fa-solid fa-check"></i> Secure payment</h3>
+                    <p>Secure payment solution by Banque Postale</p>
+                </div>
+                <div>
+                    <h3><i className="fa-solid fa-check"></i> Contact us</h3>
+                    <p>Our team is available for any questions or requests.</p>
+                </div>
+            </div>
         </div>
     )
 }
