6bc1d6b9b43ae5fa596520c3efe5561e96288ac8
Reginald Desrosiers <reginald.j.desrosiers@gmail.com>
2024-01-10 20:24:31 -0500

    styled museum products page

--- react-vite/src/components/MuseumProductsPage/MuseumProductsPage.jsx
+++ react-vite/src/components/MuseumProductsPage/MuseumProductsPage.jsx
@@ -18,18 +18,40 @@ export default function MuseumProductsPage() {
     }, [dispatch, museumId])
 
     return (
-        <div>
-            <div>
-                <p><NavLink to="/"><span> <i className="fa-solid fa-angle-left"></i> Home</span></NavLink> <NavLink to="/museums"><span>Museums</span></NavLink> {museum.length === 1 ? <NavLink to={`/museums/${museum[0].id}`}><span>{museum[0].name}</span></NavLink> : null }</p>
-                {museum.length === 1 ? <h1>{museum[0].name} Products</h1> : null}
+        <div className="all-museums-container">
+            <div className="museums-best-sellers-header">
+                <p><NavLink to="/" className='no-underline font-text'><span> <i className="fa-solid fa-angle-left"></i> Home</span></NavLink> <NavLink to="/museums" className='no-underline font-text'><span>Museums</span></NavLink> {museum.length === 1 ? <NavLink to={`/museums/${museum[0].id}`} className='no-underline font-text'><span>{museum[0].name}</span></NavLink> : null }</p>
+                {museum.length === 1 ? <h1 className="font-header all-museums-header-title">{museum[0].name} Products</h1> : null}
             </div>
-            {museumProducts && museumProducts.length > 0 && museumProducts.map((product) =>
-                <NavLink to={`/products/${product.id}/details`}><div>
-                    <img src={product?.product_images[0].image_url} />
-                    <h3>{product?.name}</h3>
-                    <p>€{product?.price}</p>
-                </div></NavLink>
-                )}
+            <div className="best-sellers-preview-grid font-text">
+                {museumProducts && museumProducts.length > 0 && museumProducts.map((product) =>
+                    <NavLink to={`/products/${product.id}/details`} className='no-underline'><div className="best-sellers-preview-container">
+                        <img src={product?.product_images[0].image_url} className="best-sellers-preview-img" />
+                        <div className="best-sellers-preview-name">
+                            <h3>{product?.name}</h3>
+                        </div>
+                        <p>€{product?.price.toFixed(2)}</p>
+                    </div></NavLink>
+                    )}
+            </div>
+            <div className="product-guarantees-infocard-container font-text">
+                <div className="product-guarantees-infocard-details">
+                    <h3><i className="fa-solid fa-check"></i> Delivery</h3>
+                    <p>Shipping in 1-2 business days according to the method of delivery chosen</p>
+                </div>
+                <div>
+                    <h3><i className="fa-solid fa-check"></i> Satisfied?</h3>
+                    <p>14 days to change your mind</p>
+                </div>
+                <div>
+                    <h3><i className="fa-solid fa-check"></i> Secure payment</h3>
+                    <p>Secure payment solution by Banque Postale</p>
+                </div>
+                <div>
+                    <h3><i className="fa-solid fa-check"></i> Contact us</h3>
+                    <p>Our team is available for any questions or requests.</p>
+                </div>
+            </div>
         </div>
     )
 }
