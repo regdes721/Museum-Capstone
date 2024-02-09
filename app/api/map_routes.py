6f40c758c8f5cb46308c4164da333557ae6f510f
Reginald Desrosiers <reginald.j.desrosiers@gmail.com>
2024-02-09 09:40:23 -0500

    started map routes (potentially wrong)

--- app/api/map_routes.py
+++ app/api/map_routes.py
@@ -1,4 +1,9 @@
-from flask import Blueprint, session, jsonify, request
+import os
+from flask import Blueprint, session, current_app, jsonify, request
 from flask_login import login_required
 
 map_routes = Blueprint('maps', __name__)
+
+@map_routes.route('/key', methods=['POST'])
+def key():
+    return jsonify({'GOOGLE_MAPS_API_KEY': current_app.config['GOOGLE_MAPS_API_KEY']})
