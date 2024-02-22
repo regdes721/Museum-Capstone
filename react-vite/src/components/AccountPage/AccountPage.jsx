c67f85ccde31c04f5b481db2dbd2921e9aa8dd26
Reginald Desrosiers <reginald.j.desrosiers@gmail.com>
2024-02-22 05:51:42 -0500

    account club details styling

--- react-vite/src/components/AccountPage/AccountPage.jsx
+++ react-vite/src/components/AccountPage/AccountPage.jsx
@@ -34,18 +34,47 @@ export default function AccountPage() {
                     <p onClick={logout} className="account-details-tab">Sign out</p>
                 </div>
                 {accountDetailsTab === 1 && <div className="account-details-home-container">
-                    <div className="account-details-home-card">
+                    <div className="account-details-home-card font-text">
+                        <img src="https://www.boutiquesdemusees.fr/img/pict-club.gif" width="119" height="90" />
                         <h2>Club Boutiques de Musées</h2>
                         <p>Collect points with your online purchases and receive loyalty vouchers</p>
                         <p>Take part in private events</p>
                         <p>Benefit from special offers</p>
                     </div>
-                    <div className="account-details-home-card">
+                    <div className="account-details-home-card font-text">
+                        <img src="https://www.boutiquesdemusees.fr/img/pict-carnet.gif" width="66" height="85" />
                         <h2>My details</h2>
                         <p>{sessionUser.first_name} {sessionUser.last_name}</p>
                         <p>{sessionUser.email}</p>
                         <p>Client Number: {sessionUser.id}</p>
                     </div>
+                </div>}
+                {accountDetailsTab === 6 && <div className="account-details-club-container font-text">
+                    <h2>Become a member and enjoy exclusive benefits</h2>
+                    <div className="account-details-club-grid">
+                        <div>
+                            <div className="account-details-club-img">
+                                <img src="https://www.boutiquesdemusees.fr/img/pict-fidelite.gif" width="62" height="54" />
+                            </div>
+                            <h3>Loyalty points</h3>
+                            <p>1 € spent* = 1 point 150 points = 1 loyalty voucher to the value of 7,50 €**</p>
+                        </div>
+                        <div>
+                            <div className="account-details-club-img">
+                                <img src="https://www.boutiquesdemusees.fr/img/pict-privilege.gif" width="54" height="77" />
+                            </div>
+                            <h3>Special events</h3>
+                            <p>Discover our know-how, mini-conferences, dedications...</p>
+                        </div>
+                        <div>
+                            <div className="account-details-club-img">
+                                <img src="https://www.boutiquesdemusees.fr/img/pict-offres.gif" width="48" height="62" />
+                            </div>
+                            <h3>Special offers</h3>
+                            <p>Enjoy special offers and advance access on products</p>
+                        </div>
+                    </div>
+                    <p>To join the loyalty program is simple, just check the corresponding box after paying for your next order.</p>
                 </div>}
             </div>
         </div>
